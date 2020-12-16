5ba73f36794f0316ed02d9f20b2e09c4b3767f28
Demont <d.zhang@effy.fr>
2020-12-16 22:34:35 +0100

    move player

--- game.py
+++ game.py
@@ -5,3 +5,4 @@ class Game:
 
     def __init__(self):
         self.player = Player()
+        self.pressed = {}
--- main.py
+++ main.py
@@ -24,6 +24,11 @@ while running:
     # apply player image
     screen.blit(game.player.image, game.player.rect)
 
+    # check if player go to right or left
+    if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x + game.player.rect.width < screen.get_width():
+        game.player.move_right()
+    elif game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0:
+        game.player.move_left()
 
     # maj l'ecran
     pygame.display.flip()
@@ -32,5 +37,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-            print('Fermer fenetre')
+            print('Close windows')
+
+        elif event.type == pygame.KEYDOWN:
+            game.pressed[event.key] = True
+        elif event.type == pygame.KEYUP:
+            game.pressed[event.key] = False
 
