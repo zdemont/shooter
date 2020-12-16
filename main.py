17a22a81850fe00f7a34cf77a13885dbe0092a16
Demont <d.zhang@effy.fr>
2020-12-16 22:08:42 +0100

    add player on the screen

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import pygame
-
+from game import Game
 pygame.init()
 
 
-
+# generate window
 pygame.display.set_caption('Comet fall Game')
 screen = pygame.display.set_mode((1080, 720))
 
+# import background window
 background = pygame.image.load('assets/bg.jpg')
+
+# load game
+game = Game()
+
 
 running = True
 
 while running:
 
-    screen.blit(background, (0, 0))
+    # apply game background
+    screen.blit(background, (0, -200))
+
+    # apply player image
+    screen.blit(game.player.image, game.player.rect)
+
 
     # maj l'ecran
     pygame.display.flip()
