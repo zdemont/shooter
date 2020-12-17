c63997f3376f20e0957faeb340637854c6011b0c
Demont <d.zhang@effy.fr>
2020-12-17 22:49:05 +0100

    projectile collision

--- projectile.py
+++ projectile.py
@@ -27,5 +27,8 @@ class Projectile(pygame.sprite.Sprite):
         self.rect.x += self.velocity
         self.rotate()
 
+        if self.player.game.check_collision(self, self.player.game.all_monsters):
+            self.remove()
+
         if self.rect.x > 1080:
             self.remove()
